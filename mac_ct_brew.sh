6c1f7e4c85e311a7ae3a9ea125d0cf3146dddd5f
i309511 <harrifeng@gmail.com>
2015-11-29 03:14:35 -0800

    new settings

---
+++ mac_ct_brew.sh
@@ -0,0 +1,10 @@
+brew install curl --with-libssh2
+brew install mysql
+brew install imagemagick
+brew install opensaml
+brew install p7zip
+brew install ffmpeg
+brew install poppler
+brew install phantomjs
+brew install unixodbc
+brew install curl --with-libssh2
---
+++ ms.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+ssh core@mo-bad691a61.mo.sap.corp
